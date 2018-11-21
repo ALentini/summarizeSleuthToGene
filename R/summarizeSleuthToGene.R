e3b30482fe0a8f73bb83a2a8373e49bc7f2dc3d5
Antonio Lentini <antonio.lentini@liu.se>
2018-11-21 15:46:03 +0100

    Update summarizeSleuthToGene.R

--- R/summarizeSleuthToGene.R
+++ R/summarizeSleuthToGene.R
@@ -1,10 +1,11 @@
 #Extracts estimated RNA-seq count data from a sleuth object and summarizes it to gene-level using the tximport algorithm
 #Uses the data.table package for speed but can be changed to base::data.frame using reshape2::dcast
-summarizeSleuthToGene <- function(sleuth_object,tx2gene, ... ){
+summarizeSleuthToGene <- function(sleuth_object,tx2gene, which_df = c("obs_raw", "obs_norm", "obs_norm_filt"), ... ){
+  which_df <- match.arg(which_df, c("obs_raw", "obs_norm", "obs_norm_filt") )
   require(sleuth)
   require(tximport)
   require(data.table)
-  dat <- data.table(sleuth_object$obs_norm)
+  dat <- data.table(sleuth_object[[which_df]])
   cnt_mat <- as.matrix(data.frame(dcast.data.table(data = dat, formula = target_id ~ sample, value.var = "est_counts"),row.names = 1))
   tpm_mat <- as.matrix(data.frame(dcast.data.table(data = dat, formula = target_id ~ sample, value.var = "tpm"),row.names = 1))
   len_mat <- as.matrix(data.frame(dcast.data.table(data = dat, formula = target_id ~ sample, value.var = "eff_len"),row.names = 1))
