6024c18e382d0c6ed3fc7f482143bcaafe4ac057
Antonio Lentini <antonio.lentini@liu.se>
2018-11-26 08:04:29 +0100

    Update summarizeSleuthToGene.R

--- R/summarizeSleuthToGene.R
+++ R/summarizeSleuthToGene.R
@@ -1,7 +1,9 @@
 #Extracts estimated RNA-seq count data from a sleuth object and summarizes it to gene-level using the tximport algorithm
-#Uses the data.table package for speed but can be changed to base::data.frame using reshape2::dcast
-summarizeSleuthToGene <- function(sleuth_object,tx2gene, which_df = c("obs_raw", "obs_norm", "obs_norm_filt"), ... ){
-  which_df <- match.arg(which_df, c("obs_raw", "obs_norm", "obs_norm_filt") )
+#Uses the data.table package for speed but can be changed to base::data.frame eg. using reshape2::dcast
+summarizeSleuthToGene <- function(sleuth_object, tx2gene, which_df = c("obs_norm_filt", "obs_norm", "obs_raw"), ... ){
+  if( class(sleuth_object) != 'sleuth' ) stop( '"sleuth_object" must be a sleuth object' )
+  if( missing(sleuth_object) ) stop( 'argument "tx2gene" is missing with no defualt' )
+  which_df <- match.arg(which_df, c("obs_norm_filt", "obs_norm", "obs_raw") )
   require(sleuth)
   require(tximport)
   require(data.table)
