bec1173aaf4c83756b160ef0d5d01444171cf5a4
Antonio Lentini <antle718@gmail.com>
2020-01-08 15:53:06 +0100

    Update summarizeSleuthToGene.R

--- R/summarizeSleuthToGene.R
+++ R/summarizeSleuthToGene.R
@@ -2,7 +2,7 @@
 #Uses the data.table package for speed but can be changed to base::data.frame eg. using reshape2::dcast
 summarizeSleuthToGene <- function(sleuth_object, tx2gene, which_df = c("obs_norm_filt", "obs_norm", "obs_raw"), ... ){
   if( class(sleuth_object) != 'sleuth' ) stop( '"sleuth_object" must be a sleuth object' )
-  if( missing(sleuth_object) ) stop( 'argument "tx2gene" is missing with no defualt' )
+  if( missing(sleuth_object) ) stop( 'argument "tx2gene" is missing with no default' )
   which_df <- match.arg(which_df, c("obs_norm_filt", "obs_norm", "obs_raw") )
   require(sleuth)
   require(tximport)
